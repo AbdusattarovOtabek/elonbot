fad4b54c41fe1384a49219e7a88c25698b894f07
Otabek <120999703+AbdusattarovOtabek@users.noreply.github.com>
2024-11-06 23:19:02 +0500

    Update 2.x settings.py

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = ("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://elonbot-vkqn.onrender.com"]
 
 
 # Application definition
